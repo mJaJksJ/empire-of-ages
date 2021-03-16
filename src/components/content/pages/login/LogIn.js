985b4f6d8c244b09523079c35a2b7b75d0120a13
m_a_ks_ <maksim.m00@mail.ru>
2021-03-16 21:48:17 +0300

    validation in log-in form

--- src/components/content/pages/login/LogIn.js
+++ src/components/content/pages/login/LogIn.js
@@ -1,47 +1,142 @@
-import React from 'react'
+import React, {Component} from 'react'
 
 import styleClasses from './LogIn.module.css'
 
-const LogIn = function () {
-    return (
-        <div className={styleClasses.LogIn}>
-            <h2>Registration form</h2>
-            <form method="get" action="/LogIn">
-                <p>
-                    <input required type="text" name="name" maxLength={20} placeholder={'name'}
-                    pattern={"^[A-Z][a-z]*"}/>
-                </p>
-                <p>
-                    <input required type="text" name="surname" maxLength={20} placeholder={'surname'}
-                    pattern={"^[A-Z][a-z]*"}/>
-                </p>
-                <p>
-                    <input required type="date" name="birthday" placeholder={'birthday'}/>
-                </p>
-                <p>
-                    <input required type="text" name="login" maxLength={20} placeholder={'login'}
-                    pattern={"[A-Za-z0-9._-]*"}/>
-                </p>
-                <p>
-                    <input required type="email" name="email" placeholder={'email'}
-                    pattern={".*@.*\\..*"}/>
-                </p>
-                <p>
-                    <input required type="password" name="password" placeholder={'password'}
-                    pattern={"(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}"}/>
-                </p>
+class LogIn extends Component {
+    patterns = {
+        name: /^[A-Z][a-z]{1,20}/,
+        surname: /^[A-Z][a-z]{1,20}/,
+        login: /[A-Za-z0-9._-]{4,10}/,
+        email: /.*@.*\..*/,
+        password: /(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}/
+    }
 
-                <p >
-                    <input type="submit" value="Log in" className={styleClasses.button}/>
-                    <input type="reset" value="Reset" className={styleClasses.button}/>
-                </p>
+    constructor(props) {
+        super(props);
+        this.state = {password: "", nameMes: ".", surnameMes: ".", loginMes: ".", emailMes: ".", passwordMes: "."};
+        this.updateNameMes = this.updateNameMes.bind(this);
+        this.updateSurnameMes = this.updateSurnameMes.bind(this);
+        this.updateLoginMes = this.updateLoginMes.bind(this);
+        this.updateEmailMes = this.updateEmailMes.bind(this);
+        this.updatePasswordMes = this.updatePasswordMes.bind(this);
+        this.updatePassword = this.updatePassword.bind(this);
+    };
 
+    updateNameMes(e) {
+        if (e.target.value.length < 1 + 1 || e.target.value.length > 20 + 1) {
+            this.setState({nameMes: "Name must have "})
+        } else if (!this.patterns.name.test(e.target.value)) {
+            this.setState({nameMes: "Name must begin with upper and other must be lower, only letters"})
+        } else {
+            this.setState({nameMes: "."})
+        }
+    };
 
+    updateSurnameMes(e) {
+        if (e.target.value.length < 1 + 1 || e.target.value.length > 20 + 1) {
+            this.setState({surnameMes: "Surname must have 2..21 syms"})
+        } else if (!this.patterns.surname.test(e.target.value)) {
+            this.setState({surnameMes: "Surname must have 2..21 syms"})
+        } else {
+            this.setState({surnameMes: "."})
+        }
+    };
 
-            </form>
+    updateLoginMes(e) {
+        if (e.target.value.length < 1 + 1 || e.target.value.length > 20 + 1) {
+            this.setState({loginMes: "Login must have 4..10 syms"})
+        } else if (!this.patterns.login.test(e.target.value)) {
+            this.setState({loginMes: "Login must have letters, nums, dot and underscore"})
+        } else {
+            this.setState({loginMes: "."})
+        }
+    };
 
-        </div>
-    );
+    updateEmailMes(e) {
+        if (!this.patterns.email.test(e.target.value)) {
+            this.setState({emailMes: "Email has name and domain name"})
+        } else {
+            this.setState({emailMes: "."})
+        }
+    };
+
+    updatePasswordMes(e) {
+        if (!this.patterns.password.test(e.target.value)) {
+            this.setState({passwordMes: "Password must have Upper letter, lower letter, nums and their more 8"})
+        } else {
+            this.setState({passwordMes: "."})
+        }
+    };
+
+    updatePassword(e) {
+        this.updatePasswordMes(e);
+        this.setState({password: e.target.value});
+    };
+
+    render() {
+        return (
+            <div className={styleClasses.LogIn}>
+                <h2>Registration form</h2>
+                <form method="get" action="/LogIn">
+                    <div>
+                        <input required type="text" name="name" placeholder={'name'}
+                               pattern={this.patterns.name.source} onChange={this.updateNameMes}/>
+                    </div>
+                    <div className={styleClasses.ValidMes}>
+                        {this.state.nameMes}
+                    </div>
+                    <div>
+                        <input required type="text" name="surname" placeholder={'surname'}
+                               pattern={this.patterns.surname.source} onChange={this.updateSurnameMes}/>
+                    </div>
+                    <div className={styleClasses.ValidMes}>
+                        {this.state.surnameMes}
+                    </div>
+                    <div>
+                        <input required type="date" name="birthday" placeholder={'birthday'}
+                               max={`${new Date().getFullYear()}-${new Date().getMonth()}-${new Date().getDay()}`}/>
+                    </div>
+                    <div className={styleClasses.ValidMes}>
+                        .
+                    </div>
+                    <div>
+                        <input required type="text" name="login" placeholder={'login'}
+                               pattern={this.patterns.login.source} onChange={this.updateLoginMes}/>
+                    </div>
+                    <div className={styleClasses.ValidMes}>
+                        {this.state.loginMes}
+                    </div>
+                    <div>
+                        <input required type="email" name="email" placeholder={'email'}
+                               pattern={this.patterns.email.source} onChange={this.updateEmailMes}/>
+                    </div>
+                    <div className={styleClasses.ValidMes}>
+                        {this.state.emailMes}
+                    </div>
+                    <div>
+                        <input required type="password" name="password" placeholder={'password'}
+                               pattern={this.patterns.password.source} onChange={this.updatePassword}
+                        />
+                    </div>
+                    <div className={styleClasses.ValidMes}>
+                        {this.state.passwordMes}
+                    </div>
+                    <div>
+                        <input required type="password" name="password" placeholder={'repeat password'}
+                               pattern={this.state.password}/>
+
+                    </div>
+
+                    <p>
+                        <input type="submit" value="Log in" className={styleClasses.button}/>
+                        <input type="reset" value="Reset" className={styleClasses.button}/>
+                    </p>
+
+                </form>
+
+            </div>
+        )
+    }
 }
 
 export default LogIn;
--- src/states/News.js
+++ src/states/News.js
@@ -1,5 +1,12 @@
 const news = [
     {
+        title: "16.03.2021 Update",
+        date: "16.03.2021",
+        text: [
+            "Add validation for log-in form"
+        ]
+    },
+    {
         title: "14.03.2021 Update",
         date: "14.03.2021",
         text: [
